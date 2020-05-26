ee487c9db36444129f99c51e3abb5a1e4593779f
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2020-05-26 16:28:17 +0430

    Add exams app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # username is national-id
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
